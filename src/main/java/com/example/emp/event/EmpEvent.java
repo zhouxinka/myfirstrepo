ecd063c3eb9366a1d82829bdcdeb7261d6760283
zhouxinka <61810813+zhouxinka@users.noreply.github.com>
2021-02-04 17:06:07 +0800

    删除代码
    删除代码

--- src/main/java/com/example/emp/event/EmpEvent.java
+++ src/main/java/com/example/emp/event/EmpEvent.java
@@ -5,8 +5,5 @@ import org.springframework.context.ApplicationEvent;
 public class EmpEvent extends ApplicationEvent {
     public EmpEvent(Object source) {
         super(source);
-        int i;
-        int j;
-        int mmm;
     }
 }
