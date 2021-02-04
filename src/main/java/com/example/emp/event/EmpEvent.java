dc0b757f7b32ff380c0798f3ff9732770d1464a1
zhouxinka <61810813+zhouxinka@users.noreply.github.com>
2021-02-04 17:11:25 +0800

    111

--- src/main/java/com/example/emp/event/EmpEvent.java
+++ src/main/java/com/example/emp/event/EmpEvent.java
@@ -5,12 +5,5 @@ import org.springframework.context.ApplicationEvent;
 public class EmpEvent extends ApplicationEvent {
     public EmpEvent(Object source) {
         super(source);
-<<<<<<< HEAD
-        int i;
-        int j;
-        int mmm;
-        int o=0;
-=======
->>>>>>> branch 'master' of https://github.com/zhouxinka/myfirstrepo.git
     }
 }
